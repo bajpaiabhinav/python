c7ac70e6daa72975a9af0ce614685fe28bee79e6
bajpaiabhinav <abhinav23153083@akgec.ac.in>
2025-04-13 16:23:51 +0000

    strings

---
+++ string.py
@@ -0,0 +1,14 @@
+name = "Abhinav Bajpai"
+Apple = '''Test is Delicious.
+color is red size is medium and 
+"shape is circular'''
+print(name[1])
+print(name[1])   # shift + Alt + down arrow key
+print(name[7])
+print(name[5])
+print(name[1])
+print(name[8])
+print("Lets use a For loop/n")
+for charactor in Apple:
+    print(charactor)           # General Syntax to print each charactor individually of string Apple....
+    
