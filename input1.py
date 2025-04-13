b81adca7129775dae3aa2503c6105ee114e6519a
bajpaiabhinav <abhinav23153083@akgec.ac.in>
2025-04-13 16:23:28 +0000

    input kaise lete hai

---
+++ input1.py
@@ -0,0 +1,10 @@
+a = input("Enter the number")
+print("my number is:", a )
+
+
+x = input("what is your name:")
+y = input("who are you:")
+print(x+y) # concatenation of two strings so when we give the input in string or int datatype then it combines 
+# example: X=Abhinav , Y=Bajpai  then X+Y = AbhinavBajpai
+# x=298 , Y=56 then X+Y = 29856
+ # suppose we want to print (int(x) + int (y))   then it will through an error in the program.....
